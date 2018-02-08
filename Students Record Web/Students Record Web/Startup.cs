1bebc773bc031341944ab01b774d445abd211e5c
Mahfuzur-Rahman <7mahfuzur.rahman@gmail.com>
2018-02-09 02:22:53 +0600

    Students Record Web Application Start

---
+++ Students Record Web/Students Record Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Students_Record_Web.Startup))]
+namespace Students_Record_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
